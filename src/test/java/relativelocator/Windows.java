2df3b519ee8f4312671cba731091f600b527ae58
snehamahat211 <snehamahat211@gmail.com>
2025-12-17 11:21:19 +0545

    search the product

--- src/test/java/relativelocator/Windows.java
+++ src/test/java/relativelocator/Windows.java
@@ -9,9 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Windows
-
-{
+public class Windows {
     WebDriver driver;
     @BeforeMethod
     public void setup()
@@ -19,9 +17,13 @@ public class Windows
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("http://www.automationpractice.pl/index.php");
     }
     @Test
     public void testWorkingInBothWindowsTabs() {
+
+        String mainFirstWindow = driver.getWindowHandle();
+
         //Automatically Open and switch to the new windows or tab
         driver.switchTo().newWindow(WindowType.TAB).get("http://www.automationpractice.pl/index.php?controller=authentication&back=my-account");
         System.out.println("Title:" + driver.getTitle());
@@ -30,6 +32,13 @@ public class Windows
         //Work in the new windows or tab
         driver.findElement(By.id("email_create")).sendKeys("Selenium4@TAU.com");
         driver.findElement(By.id("SubmitCreate")).click();
+
+        //Switch and work in the main window or tab
+        driver.switchTo().window(mainFirstWindow);
+        driver.findElement(By.id("search_query_top")).sendKeys("Shirt");
+        driver.findElement(By.name("submit_search")).click();
+        System.out.println("Title:" + driver.getTitle());
+
     }
     @AfterMethod
     public void teardown(){
