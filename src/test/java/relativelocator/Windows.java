4a3566be2081afee90c8c0605b0e128e2a5225d4
snehamahat211 <snehamahat211@gmail.com>
2025-12-17 11:05:16 +0545

    email tesy in next file

--- src/test/java/relativelocator/Windows.java
+++ src/test/java/relativelocator/Windows.java
@@ -9,23 +9,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Relative {
-    WebDriver driver;
+public class Windows
 
+{
+    WebDriver driver;
     @BeforeMethod
-    public void setup(){
+    public void setup()
+    {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://www.automationpractice.pl/index.php");
     }
-
     @Test
-    public void testNewWindowTab(){
-        WebDriver newWindow = driver.switchTo().newWindow(WindowType.WINDOW);
-        newWindow.get("http://www.automationpractice.pl/index.php?controller=stores");
-        System.out.println("Title:"+driver.getTitle());
+    public void testWorkingInBothWindowsTabs() {
+        //Automatically Open and switch to the new windows or tab
+        driver.switchTo().newWindow(WindowType.TAB).get("http://www.automationpractice.pl/index.php?controller=authentication&back=my-account");
+        System.out.println("Title:" + driver.getTitle());
 
+
+        //Work in the new windows or tab
+        driver.findElement(By.id("email_create")).sendKeys("Selenium4@TAU.com");
+        driver.findElement(By.id("SubmitCreate")).click();
     }
     @AfterMethod
     public void teardown(){
