2dd3bb229180bbde492f75921402a5fdd232049b
snehamahat211 <snehamahat211@gmail.com>
2025-12-18 15:31:57 +0545

    screenshots

--- src/test/java/relativelocator/Screenshots.java
+++ src/test/java/relativelocator/Screenshots.java
@@ -2,9 +2,14 @@ package relativelocator;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Screenshots  {
     WebDriver driver;
@@ -13,15 +18,21 @@ public class Screenshots  {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com");
+        driver.get("https://applitools.com/");
 
     }
 
     public void takeWebElementScreenshot() {
-        WebElement nextGenerationPlatform =driver.findElement(By.cssSelector("#post-8 h1"));
+        WebElement nextGenerationPlatform = driver.findElement
+                (By.id("h-fast-scalable-reliable-deterministic-ai-tests-that-never-hallucinate"));
+        File source = nextGenerationPlatform.getScreenshotAs(OutputType.FILE);
+        File Destination = new File("Next Generation.png");
+        try {
+            FileHandler.copy(source, Destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-
-
 
 }
